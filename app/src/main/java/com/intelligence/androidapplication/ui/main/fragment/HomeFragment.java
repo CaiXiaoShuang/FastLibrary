f1a1b0f4fccf0fcf73b19df93f5cf85033614801
CaiXiaoShuang <1419906976@qq.com>
2017-09-30 10:07:53 +0800

    <imp 蔡晓双>添加扫一扫框架，功能

--- app/src/main/java/com/intelligence/androidapplication/ui/main/fragment/HomeFragment.java
+++ app/src/main/java/com/intelligence/androidapplication/ui/main/fragment/HomeFragment.java
@@ -1,15 +1,29 @@
 package com.intelligence.androidapplication.ui.main.fragment;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.intelligence.androidapplication.R;
+import com.intelligence.androidlibrary.zxing.android.CaptureActivity;
 
 import butterknife.Bind;
 
+import static android.app.Activity.RESULT_OK;
+
 public class HomeFragment extends BaseFragment {
-   @Bind(R.id.tv_wo)
+    @Bind(R.id.tv_wo)
     TextView tv_wo;
+    @Bind(R.id.bt_scan)
+    Button bt_scan;
+
+    private static final String DECODED_CONTENT_KEY = "codedContent";
+    private static final String DECODED_BITMAP_KEY = "codedBitmap";
+    private static final int REQUEST_CODE_SCAN = 0x0000;
+
     public static HomeFragment newInstance() {
         Bundle args = new Bundle();
         HomeFragment fragment = new HomeFragment();
@@ -29,6 +43,28 @@ public class HomeFragment extends BaseFragment {
 
     @Override
     protected void initView() {
+        bt_scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mActivity, CaptureActivity.class);
+                startActivityForResult(intent, REQUEST_CODE_SCAN);
+            }
+        });
+
         tv_wo.setText("首页Text");
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // 扫描二维码/条码回传
+        if (requestCode == REQUEST_CODE_SCAN && resultCode == RESULT_OK) {
+            if (data != null) {
+                String content = data.getStringExtra(DECODED_CONTENT_KEY);
+                Bitmap bitmap = data.getParcelableExtra(DECODED_BITMAP_KEY);
+                tv_wo.setText("解码结果： \n" + content);
+
+            }
+        }
     }
 }
